acd923e5eb03f29fe7b3627b9f828cb3213cceda
unknown <merrill.huang@gmail.com>
2023-09-19 10:58:01 -0500

    add comment for id field in User model

---
+++ src/main/java/com/example/springbootminiproject/model/User.java
@@ -0,0 +1,13 @@
+package com.example.springbootminiproject.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;        //unique auto-generated id for each user
+}
