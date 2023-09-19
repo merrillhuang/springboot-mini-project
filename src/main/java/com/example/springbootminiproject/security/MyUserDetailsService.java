849c13aa3bd894f0df04da216e8e848dcb5e2e28
unknown <merrill.huang@gmail.com>
2023-09-19 15:31:24 -0500

    add UserService field to MyUserDetailsService

--- src/main/java/com/example/springbootminiproject/security/MyUserDetailsService.java
+++ src/main/java/com/example/springbootminiproject/security/MyUserDetailsService.java
@@ -1,4 +1,21 @@
 package com.example.springbootminiproject.security;
 
+import com.example.springbootminiproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 public class MyUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
