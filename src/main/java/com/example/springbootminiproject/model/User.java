eb77007941bf58485e34fa0887930561f344f639
unknown <merrill.huang@gmail.com>
2023-09-19 11:06:11 -0500

    add toString for User

--- src/main/java/com/example/springbootminiproject/model/User.java
+++ src/main/java/com/example/springbootminiproject/model/User.java
@@ -66,4 +66,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
