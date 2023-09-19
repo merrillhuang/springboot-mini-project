01778a85aaed6f0d09c6af41eaaf338d05eb6e65
unknown <merrill.huang@gmail.com>
2023-09-19 15:33:14 -0500

    add userRepository to UserService

--- src/main/java/com/example/springbootminiproject/service/UserService.java
+++ src/main/java/com/example/springbootminiproject/service/UserService.java
@@ -1,7 +1,14 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
