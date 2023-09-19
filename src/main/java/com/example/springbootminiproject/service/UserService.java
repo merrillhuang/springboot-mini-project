da67f178698b6213e2e78b62540687d668a8f2e2
unknown <merrill.huang@gmail.com>
2023-09-19 17:53:01 -0500

    import LoginRequest to UserService

--- src/main/java/com/example/springbootminiproject/service/UserService.java
+++ src/main/java/com/example/springbootminiproject/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.springbootminiproject.service;
 
 import com.example.springbootminiproject.exception.InformationExistException;
 import com.example.springbootminiproject.model.User;
+import com.example.springbootminiproject.model.request.LoginRequest;
 import com.example.springbootminiproject.repository.UserRepository;
 import com.example.springbootminiproject.security.JWTUtils;
 import com.example.springbootminiproject.security.MyUserDetails;
