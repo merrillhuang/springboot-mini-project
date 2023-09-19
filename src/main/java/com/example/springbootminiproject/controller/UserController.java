560b1a19b7c9374ecda8d4c01663fb1619a28469
unknown <merrill.huang@gmail.com>
2023-09-19 11:27:33 -0500

    add Hello mapping for UserController

--- src/main/java/com/example/springbootminiproject/controller/UserController.java
+++ src/main/java/com/example/springbootminiproject/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.springbootminiproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/auth/users")
 public class UserController {
 
+    @GetMapping(path = "/hello/")
+    public String hello() {
+        return "Hello";
+    }
 }
