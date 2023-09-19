43ed694aca92b3f77f3fdc8d361bf07590dff0c0
unknown <merrill.huang@gmail.com>
2023-09-19 17:57:46 -0500

    add UserService field to UserController class

--- src/main/java/com/example/springbootminiproject/controller/UserController.java
+++ src/main/java/com/example/springbootminiproject/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/auth/users")
 public class UserController {
+
+    private UserService userService;
+
+    // Creates a UserService instance
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping(path = "/hello/")   //http://localhost:9094/auth/users/hello/
     public String hello() {
