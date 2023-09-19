623d4cb7bc8d5f5c1aea75b58e1261e59f6e43df
unknown <merrill.huang@gmail.com>
2023-09-19 16:12:58 -0500

    add comment to InformationNotFoundException

--- src/main/java/com/example/springbootminiproject/exception/InformationNotFoundException.java
+++ src/main/java/com/example/springbootminiproject/exception/InformationNotFoundException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException {
 
+    //This exception is thrown when an entry matching the details passed in by the Http request is not found.
+
     public InformationNotFoundException(String message) {
         super(message);
     }
