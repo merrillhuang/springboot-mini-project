1ab1a6ab91a22945c9a91ac364e6dd4aa0c996ff
unknown <merrill.huang@gmail.com>
2023-09-19 11:39:55 -0500

    give MyUserDetails a User field

--- src/main/java/com/example/springbootminiproject/security/MyUserDetails.java
+++ src/main/java/com/example/springbootminiproject/security/MyUserDetails.java
@@ -1,11 +1,15 @@
 package com.example.springbootminiproject.security;
 
+import com.example.springbootminiproject.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
 public class MyUserDetails implements UserDetails {
+
+    private User user;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
@@ -39,5 +43,9 @@ public class MyUserDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return false;
+    }
+
+    public User getUser() {
+        return user;
     }
 }
