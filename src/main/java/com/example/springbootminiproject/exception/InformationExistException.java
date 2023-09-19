96d6ea4633ea73dce888914e99d44fa0b5a21f72
unknown <merrill.huang@gmail.com>
2023-09-19 16:10:04 -0500

    add comment to InformationExistException

--- src/main/java/com/example/springbootminiproject/exception/InformationExistException.java
+++ src/main/java/com/example/springbootminiproject/exception/InformationExistException.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException {
 
+    //This exception gets thrown when we are trying to create or update an entry in the database, and the database already has an entry
+    //that matches what the Http request is passing in.
+
     public InformationExistException(String message) {
         super(message);
     }
