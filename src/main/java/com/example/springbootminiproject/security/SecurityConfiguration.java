ca79b8e679fbf2202f59e600edd753a065cabc57
unknown <merrill.huang@gmail.com>
2023-09-19 17:38:25 -0500

    make session stateless to use Jwt token

--- src/main/java/com/example/springbootminiproject/security/SecurityConfiguration.java
+++ src/main/java/com/example/springbootminiproject/security/SecurityConfiguration.java
@@ -6,17 +6,25 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableGlobalMethodSecurity  //enables need to authorize every method before executing?
+@EnableGlobalMethodSecurity(prePostEnabled = true)  //enables need to authorize every method before executing?
 public class SecurityConfiguration {
 
     // Produces passwordEncoder objects
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // Produces JwtRequestFilter objects
+    @Bean
+    public JwtRequestFilter authJwtRequestFilter() {
+        return new JwtRequestFilter();
     }
 
     // Filters incoming requests to match certain parameters
@@ -26,8 +34,10 @@ public class SecurityConfiguration {
                 .antMatchers("/h2-console/**").permitAll()  //automatically authorizes all http requests to endpoint that begin with /h2-console
                 .anyRequest().authenticated()
                 .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable()
                 .headers().frameOptions().disable();
+        http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
