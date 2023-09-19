53e62a06eaa75a2db08d15ffd48923fbc81bb9b2
unknown <merrill.huang@gmail.com>
2023-09-19 18:23:34 -0500

    add comment to LoginRequest

--- src/main/java/com/example/springbootminiproject/model/request/LoginRequest.java
+++ src/main/java/com/example/springbootminiproject/model/request/LoginRequest.java
@@ -2,6 +2,8 @@ package com.example.springbootminiproject.model.request;
 
 public class LoginRequest {
 
+    // Represents the Http request sent by the User, contains an email address and password.
+
     private String emailAddress;
 
     private String password;
