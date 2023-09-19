fc42e5098870a4c08d6d9361e68bdfdb9dbeb10e
unknown <merrill.huang@gmail.com>
2023-09-19 11:05:51 -0500

    add getters and setters for User

--- src/main/java/com/example/springbootminiproject/model/User.java
+++ src/main/java/com/example/springbootminiproject/model/User.java
@@ -23,14 +23,47 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;       //represents each user's password, is write-only, not visible in database
 
-    //default constructor
+    //default no args constructor
     public User() {
     }
 
+    //Initializes all the fields
     public User(Long id, String username, String emailAddress, String password) {
         this.id = id;
         this.username = username;
         this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
