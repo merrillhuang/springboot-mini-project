aa70d5b03ec84d7d837cd9b262c55d9cc5f648c9
unknown <merrill.huang@gmail.com>
2023-09-19 17:21:42 -0500

    add fields to JwtRequestFilter

--- src/main/java/com/example/springbootminiproject/security/JwtRequestFilter.java
+++ src/main/java/com/example/springbootminiproject/security/JwtRequestFilter.java
@@ -1,4 +1,40 @@
 package com.example.springbootminiproject.security;
 
-public class JwtRequestFilter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    // Creates logger for this class
+    Logger logger = Logger.getLogger(JwtRequestFilter.class.getName());
+
+    private MyUserDetailsService myUserDetailsService;
+
+    private JWTUtils jwtUtils;
+
+    // Creates a MyUserDetailsService instance
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
+    // Creates a JWTUtils instance
+    @Autowired
+    public void setJwtUtils(JWTUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
