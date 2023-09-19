522d6411c90521cb8309e740cb9b06d8d1960271
unknown <merrill.huang@gmail.com>
2023-09-19 17:55:34 -0500

    fill out LoginResponse class

--- src/main/java/com/example/springbootminiproject/model/response/LoginResponse.java
+++ src/main/java/com/example/springbootminiproject/model/response/LoginResponse.java
@@ -1,4 +1,18 @@
 package com.example.springbootminiproject.model.response;
 
 public class LoginResponse {
+
+    private String jwt;
+
+    public LoginResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
 }
