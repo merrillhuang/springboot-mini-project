57f6d19713bca171582eb66dac0e6a6c83726a2a
unknown <merrill.huang@gmail.com>
2023-09-19 21:40:51 -0500

    fill out GenreController

--- src/main/java/com/example/springbootminiproject/controller/GenreController.java
+++ src/main/java/com/example/springbootminiproject/controller/GenreController.java
@@ -1,4 +1,18 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")  // http://localhost:9094/api
 public class GenreController {
+
+    private GenreService genreService;
+
+    @Autowired
+    public void setGenreService(GenreService genreService) {
+        this.genreService = genreService;
+    }
 }
