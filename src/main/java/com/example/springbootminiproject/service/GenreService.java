384bef8fdb0bc97e68a2a0333605b7c1a5aeddee
unknown <merrill.huang@gmail.com>
2023-09-19 21:27:19 -0500

    fill out GenreService

--- src/main/java/com/example/springbootminiproject/service/GenreService.java
+++ src/main/java/com/example/springbootminiproject/service/GenreService.java
@@ -1,7 +1,28 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.repository.BookRepository;
+import com.example.springbootminiproject.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class GenreService {
+
+    private GenreRepository genreRepository;
+
+    private BookRepository bookRepository;
+
+    Logger logger = Logger.getLogger(GenreService.class.getName());
+
+    @Autowired
+    public void setGenreRepository(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 }
